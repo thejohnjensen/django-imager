741af368bae13a4ca3a14f4f878f08d39dbef504
jensjoh01 <johnnyj2439@gmail.com>
2017-12-03 20:18:51 -0800

    can edit

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from django.contrib.auth.models import User
 import random
-# Create your views here.
+from django.views import generic
+from django.core.urlresolvers import reverse_lazy
+from .models import ImagerProfile
 
 
 def home_view(request):
@@ -15,7 +15,6 @@ def home_view(request):
 
 def profile_view(request):
     """."""
-    # import pdb; pdb.set_trace()
     DATA = {
         'num_photos': request.user.photo.all().count(),
         'num_albums': request.user.album.all().count(),
@@ -27,7 +26,8 @@ def profile_view(request):
         'bio': request.user.profile.bio,
         'phone': request.user.profile.phone,
         'is_active': request.user.profile.is_active,
-        'username': request.user.username
+        'username': request.user.username,
+        'pk': request.user.pk
 
     }
     return render(request, 'imager_profile/profile.html', {'DATA': DATA})
@@ -38,15 +38,24 @@ def profile_visit(request):
     username = request.path.split('/')[-1]
     if User.objects.get(username=username):
         DATA = {
-        'num_photos': request.user.photo.all().count(),
-        'num_albums': request.user.album.all().count(),
-        'camera_type': request.user.profile.camera_type,
-        'fee': request.user.profile.fee,
-        'website': request.user.profile.website,
-        'location': request.user.profile.location,
-        'services': request.user.profile.services,
-        'bio': request.user.profile.bio,
-        'phone': request.user.profile.phone,
-    }
+            'num_photos': request.user.photo.all().count(),
+            'num_albums': request.user.album.all().count(),
+            'camera_type': request.user.profile.camera_type,
+            'fee': request.user.profile.fee,
+            'website': request.user.profile.website,
+            'location': request.user.profile.location,
+            'services': request.user.profile.services,
+            'bio': request.user.profile.bio,
+            'phone': request.user.profile.phone
+        }
         return render(request, 'imager_profile/profile.html', {'DATA': DATA})
-        
+
+
+class UpdateProfile(generic.UpdateView):
+    """."""
+    model = ImagerProfile
+    template_name = 'imager_profile/update_profile.html'
+    fields = ['website', 'fee', 'camera_type', 'services', 'bio', 'phone', 'photo_styles']
+    success_url = reverse_lazy('library')
+
+
