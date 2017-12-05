d7493d6f1acd6b808b52f05747015594c1beaa3c
jensjoh01 <johnnyj2439@gmail.com>
2017-12-04 18:17:20 -0800

    working chnaging over the views to class views

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from imager_profile import views
 from django.conf.urls.static import static
-from imagersite import settings
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -15,4 +15,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
     )
